114231d67a938c8285460aa86e32faeef74c79d0
Steppenw <s.pagani73@gmail.com>
2021-09-12 19:21:43 +0200

    Commit 210912

--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Guests
+Route::get('/', 'HomeController@index');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home'); //Generata da 'php artisan ui bootstrap --auth'
+//Route::get('/home', 'HomeController@index')->name('home'); Generata da 'php artisan ui bootstrap --auth'
 
 //Admin
 Route::prefix('admin')
